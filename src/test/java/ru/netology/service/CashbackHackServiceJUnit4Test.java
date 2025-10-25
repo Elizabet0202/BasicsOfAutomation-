3e98d48c9615f6a8c6ac909f1fdd6739d92c5c9a
Elizaveta <yelizaveta.baklanova@gmail.com>
2025-10-25 17:49:08 +0300

    TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
@@ -3,13 +3,25 @@ package ru.netology.service;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.Callable;
+
 public class CashbackHackServiceJUnit4Test {
 
 @Test
-    public void testRemain() {
+    public void testRemainPositive() {
     CashbackHackService service = new CashbackHackService();
-    Assert.assertEquals(900, service.remain(100));
-    Assert.assertEquals(1000, service.remain(1000));
-    Assert.assertEquals(999, service.remain(1));
+    Assert.assertEquals(service.remain(900), 100);
+}
+
+@Test
+        public void testRemainBoundary() {
+    CashbackHackService service = new CashbackHackService();
+    Assert.assertEquals(service.remain(1000), 0); // ожидаем ошибку из-за дефекта
+}
+
+@Test
+        public void testRemainBelowBoundary() {
+    CashbackHackService service = new CashbackHackService();
+     Assert.assertEquals(service.remain(999), 1);
    }
 }
